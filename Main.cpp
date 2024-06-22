5f3dd18d96eb9e603c26ac5325b0bc242b26f0dd
Paulo Henrique Ferreira Grillo <paulo2.grillo@gmail.com>
2024-06-21 21:10:13 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,6 @@
 #include "Sistema.hpp"
-
 int main() {
-	Sistema sistema;
+	Sistema sistema;/// Cria um objeto da classe sistema responsável por gerenciar as operações de reserva e interação dos produtos.
 	int mainMenuOpt;
 	bool leave = false;
 	std::string name;
@@ -9,8 +8,8 @@ int main() {
 	std::cout << ">> ";
 	std::cin >> name;
 	std::cout << std::endl;
-	Cliente cliente(name);
-	sistema.loadStoreFromFile("ProductList.txt");
+	Cliente cliente(name);///Cria um objeto da classe Cliente com o nome fornecido pelo usuário durante a execução do programa.
+	sistema.loadStoreFromFile("ProductList.txt"); ///Chama o método loadStoreFromFile() do objeto sistema para carregar informações sobre produtos de um arquivo chamado "ProductList.txt". Esses produtos incluem passagens de avião e quartos de hotel disponíveis para reserva.
 	while (!leave) {
 		Sistema::printSectionDelimiter();
 		std::cout << std::endl << "menu principal" << std::endl << std::endl;
@@ -24,30 +23,37 @@ int main() {
 		mainMenuOpt = -1;
 		Sistema::readInteger(mainMenuOpt, 0, 5);
 		switch (mainMenuOpt) {
+			///Define leave como true, indicando que o usuário deseja sair do programa.
 			case 0:
 				std::cout << std::endl << "voce saiu do programa" << std::endl;
 				leave = true;
 				break;
+			///Chama o método addToCart() do objeto cliente para adicionar uma passagem de avião ao carrinho, usando o método pickFlyTicket() do objeto sistema para selecionar a passagem.
 			case 1:
 				Sistema::printSectionDelimiter();
 				cliente.addToCart(sistema.pickFlyTicket());
 				break;
+			///Chama o método addToCart() do objeto cliente para adicionar um quarto de hotel ao carrinho, usando o método pickHotelRoom() do objeto sistema para selecionar o quarto.
 			case 2:
 				Sistema::printSectionDelimiter();
 				cliente.addToCart(sistema.pickHotelRoom());
 				break;
+			///Chama o método removeFromCart() do objeto cliente para remover um item específico do carrinho.
 			case 3:
 				Sistema::printSectionDelimiter();
 				cliente.removeFromCart();
 				break;
+			///Chama o método dispCart(BOTH) do objeto cliente para exibir o conteúdo completo do carrinho (passagens e quartos de hotel).
 			case 4:
 				Sistema::printSectionDelimiter();
 				cliente.dispCart(BOTH);
 				break;
+			///Chama o método checkOut() do objeto cliente para finalizar a compra, exibindo um resumo da compra e permitindo a confirmação da compra pelo usuário.
 			case 5:
 				Sistema::printSectionDelimiter();
 				cliente.checkOut();
 				break;
+			///Caso o usuário digite uma opção inválida (fora do intervalo 0 a 5), exibe uma mensagem indicando que a opção não corresponde a nenhuma ação válida.
 			default:
 				std::cout << "o numero digitado nao corresponde a nenhuma acao" << std::endl;
 		}
